10682aeadf06afe1893a885a814fb848c273a9cc
Manohar_$®!_Vikram <38721567+srivikram@users.noreply.github.com>
2023-04-26 12:49:38 -0500

    login_register_index_account_commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,74 @@
 var express = require('express');
+var passport = require('passport');
 var router = express.Router();
+var Account = require('../models/account');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+  res.render('index', { title: 'Hats App', user: req.user });
 });
 
-module.exports = router;
+router.get('/register', function (req, res) {
+  res.render('register', { title: 'Hats App Registration' });
+});
+
+router.post('/register', function (req, res) {
+  Account.findOne({ username: req.body.username }) // Removed callback function
+    .then((user) => { // Handle the result with a Promise
+      if (user) {
+        return res.render('register', {
+          title: 'Registration',
+          message: 'Existing User', account: req.body.username
+        })
+      }
+      let newAccount = new Account({ username: req.body.username });
+      Account.register(newAccount, req.body.password, function (err, user) {
+        if (err) {
+          return res.render('register', {
+            title: 'Registration',
+            message: 'access error', account: req.body.username
+          })
+        }
+        if (!user) {
+          return res.render('register', {
+            title: 'Registration',
+            message: 'access error', account: req.body.username
+          })
+        }
+        console.log('Sucess, redirect');
+        res.redirect('/');
+      })
+    })
+    .catch((err) => { // Handle errors with a Promise
+      return res.render('register', {
+        title: 'Registration',
+        message: 'Registration error', account: req.body.username
+      })
+    })
+});
+
+router.get('/login', function (req, res) {
+  res.render('login', { title: 'Hats App Login', user: req.user });
+});
+
+router.post('/login', passport.authenticate('local'), function (req, res) {
+  if (req.session.returnTo)
+    res.redirect(req.session.returnTo);
+  res.redirect('/');
+});
+
+router.get('/logout', function (req, res, next) {
+
+  req.logout(function (err) {
+
+    if (err) { return next(err); }
+
+    res.redirect('/');
+
+  });
+});
+
+router.get('/ping', function (req, res) {
+  res.status(200).send("pong!");
+});
+
+module.exports = router; 
